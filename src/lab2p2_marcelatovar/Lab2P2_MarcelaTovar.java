7e494fac1be30dd258473cf2be9b83609454bf33
Marcela Tovar <marcelatovar.flores@gmail.com>
2023-01-27 14:35:59 -0600

    Creacion de menu principal

--- src/lab2p2_marcelatovar/Lab2P2_MarcelaTovar.java
+++ src/lab2p2_marcelatovar/Lab2P2_MarcelaTovar.java
@@ -10,7 +10,40 @@ public class Lab2P2_MarcelaTovar {
     }
     
     public static void menu(){
-        
+        Scanner leer = new Scanner(System.in);
+        boolean terminar = true;
+        boolean log = false;
+        do{
+            System.out.println("1.Registro de Inmueble/Solar\n2.Manejo de Estados\n3.Login/Sign-up\n4.Salir");
+            int opcion = leer.nextInt();
+            switch(opcion){
+                case 1 ->{
+                    if (log) {
+                        
+                    }else{
+                        System.out.println("Porfavor ingrese la sesion");
+                    }
+                }
+                case 2->{
+                    if (log) {
+                        
+                    }else{
+                        System.out.println("Profavor ingrese la sesion");
+                    }
+                }
+                case 3->{
+                    log = true;
+                }
+                case 4->{
+                    if (log) {
+                        terminar = false;
+                    }else{
+                        System.out.println("Porfavor ingrese la sesion");
+                    }
+                    
+                }
+            }
+        }while(terminar);
     }
     
 }
